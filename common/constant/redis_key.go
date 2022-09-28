67117837673c92b9a06da33ea791ec127e4a5d33
孙光勇 <sunguangyong@cointiger.com>
2022-09-28 19:33:04 +0800

    fix

--- common/constant/redis_key.go
+++ common/constant/redis_key.go
@@ -1,10 +1,9 @@
 package constant
 
 const (
-	LOGIN_TOKENS             = "login_tokens:"
+	LOGIN_TOKENS             = "operate_login_tokens:"
 	REDIS_PRI_RATE_LIMIT_KEY = "verification:prilimit:path:%s:apikey:%s"
 	REDIS_PUB_RATE_LIMIT_KEY = "verification:publimit:path:%s:ip:%s"
 	REDIS_USERAPIKEY_UID     = "verification:userapikey:uid:%s"
 	LOGIN_USER_ID    =  "operate_login_user_id"
-
 )
