402fbb73150441931e7eebe4dc9ea860b0b9f6a1
sunguangyong <5250105+sunguangyong@user.noreply.gitee.com>
2023-09-26 17:16:31 +0800

    gateway 添加鉴权

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -3,8 +3,11 @@ package main
 import (
 	"flag"
 
+	"cointiger.com/verification/common/middleware"
+
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/gateway"
+
 )
 
 var configFile = flag.String("f", "etc/gateway.yaml", "config file")
@@ -14,6 +17,8 @@ func main() {
 	var c gateway.GatewayConf
 	conf.MustLoad(*configFile, &c)
 	gw := gateway.MustNewServer(c)
+	// 鉴权
+	gw.Use(middleware.JwtAuth)
 	defer gw.Stop()
 	gw.Start()
 }
